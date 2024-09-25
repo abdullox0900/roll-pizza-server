73b2b855208b218cb2cf56c02a9a4a8620c934a0
abdullox0900 <abdullox0900@gmail.com>
2024-09-26 04:35:54 +0500

    feat: added env

--- server.js
+++ server.js
@@ -38,6 +38,8 @@ app.get('/image/:id', async (req, res) => {
     }
 })
 
+console.log('MONGODB_URI:', process.env.MONGODB_URI)
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
